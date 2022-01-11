6a7d16142722b149448d6ca6285a6d864e2a4135
Kunal Bagaria <kb24x7@gmail.com>
2022-01-11 23:10:56 +0530

    fix: styling and toasts

--- layouts/Head.tsx
+++ layouts/Head.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const DefaultHead = ({ title }: Props) => (
 	<Head>
-		<title>{title ? title : 'pfpStack - Your streamlined NFT avatar' }</title>
+		<title>{title ? title : 'nftpic - Your streamlined NFT avatar' }</title>
 		<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 		<link rel="icon" href="/favicon.ico" />
 	</Head>
--- pages/update.tsx
+++ pages/update.tsx
@@ -10,20 +10,14 @@ import {
 	Skeleton,
 	ImageList
 } from '@mui/material'
+import toast from 'react-hot-toast'
+import { toasterPromise } from '../components/toasterNetworkPromise'
 import { connectWallet, signMessage } from '../components/wallet'
 import { getSolanaNFTs, fetchSolanaNftImage } from '../components/getNFT'
 import { DefaultHead } from '../layouts/Head'
 import { Navbar } from '../layouts/Navbar'
 import styles from '../styles/Update.module.css'
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
-import MuiAlert, { AlertProps } from '@mui/material/Alert';
-
-const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
-	props,
-	ref,
-) {
-	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
 
 const Update: NextPage = () => {
 	const [publicKey, setPublicKey] = useState('')
@@ -33,14 +27,12 @@ const Update: NextPage = () => {
 	const [success, setSuccess] = useState(false)
 	const [error, setError] = useState(false)
 
-	console.log(image)
-
 	const getUser = async (pKey: string) => {
 		try {
 			const request = await fetch(`/api/get/user/${pKey}`)
 			const response = await request.json()
-			if (response.name) setName(response.name)
-			if (response.image) setImage(response.image)
+			if (!name && response.name) setName(response.name)
+			if (!image && response.image) setImage(response.image)
 		} catch (e) {
 			console.error(e)
 		}
@@ -60,6 +52,7 @@ const Update: NextPage = () => {
 	}
 
 	const update = async () => {
+		if (!image) return toast.error('Please select an image')
 		const signature = await signMessage('$PFPSTACK')
 		if (!signature) return
 		const data = {
@@ -69,18 +62,17 @@ const Update: NextPage = () => {
 			image
 		}
 		console.log(data)
-		const request = await fetch(`/api/update/user`, {
+		const request = fetch(`/api/update/user`, {
 			method: 'POST',
 			body: JSON.stringify(data)
 		})
-		const response = await request.json()
-		if (request.ok) {
-			setSuccess(true)
-			setTimeout(() => setSuccess(false), 3000)
-		} else {
-			setError(true)
-			setTimeout(() => setError(false), 3000)
-		}
+		toast.promise(toasterPromise(request), {
+			loading: 'Updating user',
+			success: 'User updated',
+			error: 'Error updating user'
+		})
+		const aRequest = await request
+		const response = await aRequest.json()
 		console.log(response)
 	}
 
@@ -94,12 +86,6 @@ const Update: NextPage = () => {
 				</Typography>
 
 				<div style={{ marginTop: '2rem' }}>
-					{success && (
-						<Alert severity="success">Successfully updated user!</Alert>
-					)}
-					{error && (
-						<Alert severity="error">Error updating user!</Alert>
-					)}
 					{!publicKey && (
 						<Button startIcon={<AccountBalanceWalletIcon />}
 							color="primary" onClick={connect} variant={'contained'}>Connect Wallet</Button>
@@ -108,27 +94,23 @@ const Update: NextPage = () => {
 						<div style={{ marginBottom: '6rem' }}>
 							<TextField onChange={(e) => setName(e.target.value)} value={name} color="primary" sx={{ width: 200 }} variant="outlined" label="Name (optional)" />
 							<Typography variant="body1" color="primary" marginTop={4} gutterBottom>Choose an NFT as your profile picture</Typography>
-							<ImageList variant="quilted" cols={3} rowHeight={317}>
+							<div className={styles.nftGrid}>
 								{nfts.length > 0 ? (
 									<>
 										{nfts.map((nft, index) => (
-											<ImageListItem onClick={() => setImage(nft)} key={index}>
-												<img className={styles.nft} style={{
-													cursor: 'pointer', transition: '.2s linear', borderRadius: '1rem', border: nft === image ? '3px solid #1976d2' : 'none'
-												}} src={nft} alt="" key={index} loading="lazy" />
-											</ImageListItem>
+											<img className={styles.nft} style={{
+												outline: nft === image ? '3px solid #1976d2' : 'none'
+											}} onClick={() => setImage(nft)} key={index} src={nft} alt="" loading="lazy" />
 										))}
 									</>
 								) : (
 									<>
-										{[...Array(6)].map((_, index) => (
-											<ImageListItem key={index}>
-												<Skeleton animation="wave" variant="rectangular" width={317} height={317} />
-											</ImageListItem>
+										{[...Array(3)].map((_, index) => (
+											<Skeleton key={index} animation="wave" variant="rectangular" width={160} height={160} />
 										))}
 									</>
 								)}
-							</ImageList>
+							</div>
 							<Button onClick={update} color="primary" variant={'contained'}>Update Profile</Button>
 						</div>
 					)}
